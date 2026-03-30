33f8e60f1c80de751b5ac571ac20bfa378b0d4fe
karan patel <karanpatel6898@gmail.com>
2026-03-30 16:35:38 +0530

    ocrinui

--- Resume_screener/UIafterOCR.py
+++ Resume_screener/UIafterOCR.py
@@ -2,6 +2,8 @@ import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pdfminer.high_level
+from PIL import Image
+import pytesseract
 
 # ------------------ FUNCTION TO EXTRACT TEXT ------------------
 def extract_text_from_pdf(uploaded_file):
@@ -12,14 +14,38 @@ def extract_text_from_pdf(uploaded_file):
         return ""
 
 # ------------------ STREAMLIT UI ------------------
-st.set_page_config(page_title="AI Resume Screener", layout="centered")
-st.title("AI Resume Screener")
-
+st.markdown("""
+<div style="
+    text-align: center;
+    background-color: #f0f2f6;
+    padding: 20px;
+    border-radius: 10px;
+    border: 2px solid #ddd;
+    margin-bottom: 20px;
+">
+    <h1 style="color:#333;">AI Resume Screener</h1>
+</div>
+""", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 
 with col1:
     uploaded_file = st.file_uploader("Upload Resume", type=["pdf"])
     job_desc = st.text_area("Job Description")
+    jd_option = st.radio("Job Description Input Type", ["Text", "Image"])
+    job_desc = ""
+
+if jd_option == "Text":
+    job_desc = st.text_area("Enter Job Description")
+
+else:
+    uploaded_image = st.file_uploader("Upload Job Description Image", type=["png", "jpg", "jpeg"])
+    
+    if uploaded_image:
+        image = Image.open(uploaded_image)
+        job_desc = pytesseract.image_to_string(image)
+
+        st.subheader("Extracted Text")
+        st.write(job_desc[:500])
 
 # Default values
 resume_text = ""
